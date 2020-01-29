833c664b27e22b660d0173e5acb2eaf7e4e55fcc
Jon Godin <jon.m.godin@gmail.com>
2020-01-29 13:54:00 -0800

    fix: fixing GreatOldOne mechanic

--- src/main/java/entity/cards/GreatOldOne.java
+++ src/main/java/entity/cards/GreatOldOne.java
@@ -2,23 +2,12 @@ package entity.cards;
 
 import static entity.EntityMod.makeCardPath;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
-import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.status.Burn;
-import com.megacrit.cardcrawl.cards.status.Dazed;
-import com.megacrit.cardcrawl.cards.status.Slimed;
-import com.megacrit.cardcrawl.cards.status.VoidCard;
-import com.megacrit.cardcrawl.cards.status.Wound;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.ArtifactPower;
 import entity.EntityMod;
 import entity.characters.Entity;
-import java.util.Iterator;
 
 //Frenetic Find	rare	skill	1(0)	Gain 2(3) artifact, gain a slimed, void, dazed, burn, and wound card into your discard
 public class GreatOldOne extends AbstractDynamicCard {
@@ -33,24 +22,42 @@ public class GreatOldOne extends AbstractDynamicCard {
     private static final int COST = 3;
     private static final int UPGRADED_COST = 2;
 
+    public boolean playedFromExhaust = false;
+
     public GreatOldOne() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         this.exhaust = true;
+        this.playedFromExhaust = false;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        // Exhaust all cards.
-        Iterator<AbstractCard> iterator = p.exhaustPile.group.iterator();
-        AbstractCard card;
-        while(iterator.hasNext()) {
-            card = iterator.next();
-            if (card.cardID.equals(GreatOldOne.ID)) {
-                continue;
+        // Iterate through cards as many times as we see GreatOldOne in the Exhaust pile + 1
+        // Play each card if it isn't GreatOldOne by making a stat equivalent copy. These copies disappear on use.
+        // Use booleans to keep track of which GreatOldOnes we've already seen.
+        int greatOldOnesToPlay = 1;
+        while (greatOldOnesToPlay >= 1) {
+            // Exhaust all cards.
+            for (AbstractCard card : p.exhaustPile.group) {
+                if (card.cardID.equals(GreatOldOne.ID)) {
+                    if (!((GreatOldOne) card).playedFromExhaust) {
+                        greatOldOnesToPlay++;
+                        ((GreatOldOne) card).playedFromExhaust = true;
+                    }
+                    continue;
+                }
+                AbstractMonster mo = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.monsterRng);
+                AbstractCard tmp = card.makeStatEquivalentCopy();
+                tmp.use(p, mo);
             }
-            AbstractMonster mo = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.monsterRng);
-            AbstractCard tmp = card.makeStatEquivalentCopy();
-            tmp.use(p, mo);
+            greatOldOnesToPlay--;
+        }
+
+        // Since we're done playing cards from the exhaust pile, we should reset the state of each GreatOldOne.
+        for (AbstractCard card : p.exhaustPile.group) {
+            if (card.cardID.equals(GreatOldOne.ID)) {
+                ((GreatOldOne) card).playedFromExhaust = false;
+            }
         }
     }
 
